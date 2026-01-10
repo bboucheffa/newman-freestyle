9896be192a281e5b11f62c9d560bf69d2e7db2cb
Boucheffa <boucheffa_badis@outlook.com>
2026-01-10 21:11:29 +0100

    update script file

--- newman_run.sh
+++ newman_run.sh
@@ -3,18 +3,18 @@
 set -e   # arrêter le script à la première erreur
 
 # Mise à jour du système
-sudo apt-get update -y
+apt-get update -y
 
 # Installation de Node.js
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 
 # Vérification Node & npm 
 node -v
 npm -v
 
 # Installation de Newman 
-sudo npm install -g newman
+npm install -g newman
 
 # Lancement de Newman 
 newman run collection.json
